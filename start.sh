d12098fabf5db4c50cb664dc2b43f35031dbc03f
kentquirk <kentquirk@gmail.com>
2013-12-05 09:21:18 -0800

    move environment to a proper config

--- start.sh
+++ start.sh
@@ -1,9 +1,1 @@
-export HUBOT_IRC_SERVER=irc.freenode.net
-export HUBOT_IRC_ROOMS="#tidebottest"
-export HUBOT_IRC_NICK="tidebot"
-export HUBOT_IRC_UNFLOOD="true"
-export HUBOT_GITHUB_REPO=tidepool-org/blip
-export HUBOT_GITHUB_TOKEN=0eb109552b8db45d32d6317f806a731bb09ba6aa
-export HUBOT_GITHUB_ORG=tidepool-org
-export HUBOT_HANGOUT_URL=https://plus.google.com/hangouts/_/calendar/aG93YXJkQHRpZGVwb29sLm9yZw.qbqeltj6lsalm62folsprhtado
 bin/hubot -a irc --name tidebot
